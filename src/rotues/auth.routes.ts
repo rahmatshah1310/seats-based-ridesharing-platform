c4caf2289f829e2dd3bfa8950e2d90203aed0c86
rahmatshah1310 <rahmatshah1310@gmail.com>
2026-01-26 23:55:29 +0500

    refactor auth.routes.ts to improve code structure and readability

--- src/rotues/auth.routes.ts
+++ src/rotues/auth.routes.ts
@@ -1,18 +1,53 @@
 import express from "express";
-import UserController from "../controllers/user.controller.js";
+import { registerUser, login, allUsers, getUsersByRole, deleteUser, getUserProfile, get_me, driverApprove } from "../controllers/user.controller.js";
 import { registrationUpload } from "@/middlewares/upload.mw.js";
 import { authorizeRoles, ensureAuthenticated } from "@/middlewares/auth.mw.js";
 
 const router = express.Router();
 
-router.post("/register",registrationUpload.fields([
+router.post(
+  "/register",
+  registrationUpload.fields([
     { name: "profileImage", maxCount: 1 },
     { name: "vehicleImages", maxCount: 5 },
-  ]), UserController.registerUser);
-router.post("/login",UserController.login);
-router.get("/all",ensureAuthenticated,authorizeRoles("admin"), UserController.allUsers);
-router.get("/role/:role", ensureAuthenticated,authorizeRoles("admin"), UserController.getUsersByRole);
-router.delete("/delete/:id", ensureAuthenticated,authorizeRoles("admin","passenger"), UserController.deleteUser);
-router.get("/profile/:id", ensureAuthenticated,authorizeRoles("admin","passenger","driver"), UserController.getUserProfile);
-router.get("/me", ensureAuthenticated,authorizeRoles("admin","passenger","driver"), UserController.get_me);
+  ]),
+  registerUser,
+);
+router.post("/login", login);
+router.get(
+  "/all",
+  ensureAuthenticated,
+  authorizeRoles("admin"),
+  allUsers,
+);
+router.get(
+  "/role/:role",
+  ensureAuthenticated,
+  authorizeRoles("admin"),
+  getUsersByRole,
+);
+router.delete(
+  "/delete/:id",
+  ensureAuthenticated,
+  authorizeRoles("admin", "passenger"),
+  deleteUser,
+);
+router.get(
+  "/profile/:id",
+  ensureAuthenticated,
+  authorizeRoles("admin", "passenger", "driver"),
+  getUserProfile,
+);
+router.get(
+  "/me",
+  ensureAuthenticated,
+  authorizeRoles("admin", "passenger", "driver"),
+  get_me,
+);
+router.put(
+  "/driver/approve/:id",
+  ensureAuthenticated,
+  authorizeRoles("admin"),
+  driverApprove,
+);
 export default router;
