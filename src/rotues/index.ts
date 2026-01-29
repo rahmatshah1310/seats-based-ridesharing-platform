2c73dd0bea81e6d5c1ad00074d7e56aca62385fb
rahmatshah1310 <rahmatshah1310@gmail.com>
2026-01-29 23:06:43 +0500

    add ride routes to main router

--- src/rotues/index.ts
+++ src/rotues/index.ts
@@ -1,6 +1,8 @@
 import Express from "express";
 const router = Express.Router();
 import authRotues from "./auth.routes";
+import rideRoutes from "./ride.routes";
 
 router.use("/auth", authRotues);
+router.use("/rides", rideRoutes);
 export default router;
