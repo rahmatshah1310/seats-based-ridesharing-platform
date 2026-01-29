2a7c0469f5f64c6ed2bb3dd232b860f92a8da091
rahmatshah1310 <rahmatshah1310@gmail.com>
2026-01-29 23:05:54 +0500

    refactor createRideController: enhance validation and streamline ride data handling

--- src/controllers/rides.controller.ts
+++ src/controllers/rides.controller.ts
@@ -2,26 +2,30 @@ import { createRide } from "@/services/rides.service";
 import type { Controller } from "@/db/types/controller";
 import type { ResponseWithHelpers } from "@/middlewares/response.mw";
 import type { CreateRideInput } from "@/db/types/rides.types";
+import { createRideSchema } from "../validations/rides.schema";
 
-
-export const createRideController:Controller=async(req,res)=>{
-    const r = res as ResponseWithHelpers;
-    try {
-
-        const {userId}=req.user._id;
-        const rideData:CreateRideInput={
-            driverId:req.body.driverId,
-            vehicleId:req.body.vehicleId,
-            from:req.body.from,
-            to:req.body.to,
-            departureTime:req.body.departureTime,
-            availableSeats:req.body.availableSeats,
-            bookedSeats:req.body.bookedSeats,
-        }
-        const ride=await createRide(rideData);
-        return r.success(ride, "Ride created successfully");
-    } catch (error) {
-        console.error("RidesController [createRideController] Error:", error);
-        r.serverError(error);
+export const createRideController: Controller = async (req, res) => {
+  const r = res as ResponseWithHelpers;
+  try {
+    const results = await createRideSchema.validateAsync(req.body);
+    console.log(results, "==============================>");
+    const userId = Number(req.user?._id);
+    console.log(userId, "userid================>");
+    if (!userId) {
+      return res.fail({ message: "User ID not found" });
     }
-}
+    const rideData: CreateRideInput = {
+      driverId: userId,
+      from: results.from,
+      to: results.to,
+      date: results.date,
+      time: results.time,
+      availableSeats: results.availableSeats,
+    };
+    const ride = await createRide(rideData);
+    return r.success(ride, "Ride created successfully");
+  } catch (error) {
+    console.error("RidesController [createRideController] Error:", error);
+    r.serverError(error);
+  }
+};
