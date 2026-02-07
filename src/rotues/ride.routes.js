943a72d9e15e22e523785028ba9c7a602943c485
rahmatshah1310 <rahmatshah1310@gmail.com>
2026-02-07 23:27:33 +0500

    Add routes for retrieving and updating rides: implement getAllRides and updateRide endpoints in ride.routes.js

--- src/rotues/ride.routes.js
+++ src/rotues/ride.routes.js
@@ -1,13 +1,29 @@
 import express from "express";
 const router = express.Router();
-import { createRideController } from "@/controllers/rides.controller";
+import {
+  createRideController,
+  getallRidesController,
+  updateRideController,
+} from "@/controllers/rides.controller";
 import { authorizeRoles, ensureAuthenticated } from "@/middlewares/auth.mw.js";
 
+router.get(
+  "/allRides",
+  ensureAuthenticated,
+  authorizeRoles("driver"),
+  getallRidesController,
+);
 router.post(
   "/createRide",
   ensureAuthenticated,
   authorizeRoles("driver"),
   createRideController,
+);
+router.put(
+  "/updateRide/:rideId",
+  ensureAuthenticated,
+  authorizeRoles("driver"),
+  updateRideController,
 );
 
 export default router;
