e8f78ab5a511b5e6a0dcf6e30bd9e59857a1b23d
rahmatshah1310 <rahmatshah1310@gmail.com>
2026-02-13 20:30:14 +0500

    feat: Implement core backend infrastructure including Drizzle ORM, database migrations, routing, controllers, services, and middleware for ride management.

---
+++ src/rotues/request.routes.ts
@@ -0,0 +1,53 @@
+import express from "express";
+const router = express.Router();
+import {
+  createRideRequestController,
+  deleteRideRequestController,
+  getAllRideRequestsController,
+  getRideRequestByIdController,
+  requestToSpecificRideController,
+  updateRideRequestController,
+} from "@/controllers/rideRequest.controller";
+import { authorizeRoles, ensureAuthenticated } from "@/middlewares/auth.mw.js";
+
+router.get(
+  "/getAllRideRequests",
+  ensureAuthenticated,
+  authorizeRoles("passenger"),
+  getAllRideRequestsController,
+);
+
+router.get(
+  "/getRideRequestById/:id",
+  ensureAuthenticated,
+  authorizeRoles("passenger"),
+  getRideRequestByIdController,
+);
+
+router.post(
+  "/createRideRequest",
+  ensureAuthenticated,
+  authorizeRoles("passenger"),
+  createRideRequestController,
+);
+
+router.post(
+  "/requestToSpecificRide/:id",
+  ensureAuthenticated,
+  authorizeRoles("passenger"),
+  requestToSpecificRideController,
+);
+router.put(
+  "/updateRideRequest/:id",
+  ensureAuthenticated,
+  authorizeRoles("passenger"),
+  updateRideRequestController,
+);
+
+router.delete(
+  "/deleteRideRequest/:id",
+  ensureAuthenticated,
+  authorizeRoles("passenger"),
+  deleteRideRequestController,
+);
+export default router;
