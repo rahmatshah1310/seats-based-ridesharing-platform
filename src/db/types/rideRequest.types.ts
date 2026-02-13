68ee3cac7f1681c9ca8d3c6282603f593a8540ed
rahmatshah1310 <rahmatshah1310@gmail.com>
2026-02-13 20:29:09 +0500

    feat: Set up initial backend API and database schema for ride and ride request management.

--- src/db/types/rideRequest.types.ts
+++ src/db/types/rideRequest.types.ts
@@ -1,5 +1,12 @@
 import type { rideRequestStatusEnum } from "../schema/enums";
 
+export interface PassengerInfo {
+  passengerId: string;
+  name: string;
+  profileImage?: string;
+  phone?: string;
+}
+
 export interface CreateRideRequestInput {
   from: string;
   to: string;
@@ -7,7 +14,13 @@ export interface CreateRideRequestInput {
   time: string;
   requiredSeats: number;
   bookedSeats: number;
-  passengers: number[];
+  passengerId: string;
+  passengers: PassengerInfo[];
   distanceKm: number;
   status: (typeof rideRequestStatusEnum.enumValues)[number];
+}
+
+export interface RequestToSpecificRideInput {
+  rideId: string;
+  requiredSeats: number;
 }
