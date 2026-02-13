0d6a69df5ff5eb86d03d9f24d8b12230febeb580
rahmatshah1310 <rahmatshah1310@gmail.com>
2026-02-13 20:31:01 +0500

    feat: Implement initial backend structure with Drizzle ORM migrations, routes, services, and controllers.

--- src/db/types/rides.types.ts
+++ src/db/types/rides.types.ts
@@ -1,18 +1,10 @@
 import type { rideStatusEnum } from "../schema/enums";
 
 export interface CreateRideInput {
-  driverId: number;
-  vehicleId: number;
+  driverId: string;
   from: string;
   to: string;
   date: string;
   time: string;
   availableSeats: number;
-  bookedSeats: number;
-  passengers: number[];
-  distanceKm: number;
-  status: (typeof rideStatusEnum.enumValues)[number];
-  cancellationReason: string;
-  cancelledAt: string;
-  cancelledBy: number;
 }
