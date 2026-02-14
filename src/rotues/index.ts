399790168622080e089c55d25e5756c97307a388
rahmatshah1310 <rahmatshah1310@gmail.com>
2026-02-14 22:55:45 +0500

    feat: Implement core ride and ride request management with database migrations, controllers, services, routes, and middleware.

--- src/rotues/index.ts
+++ src/rotues/index.ts
@@ -2,7 +2,9 @@ import Express from "express";
 const router = Express.Router();
 import authRotues from "./auth.routes";
 import rideRoutes from "./ride.routes";
+import requestRoutes from "./request.routes";
 
 router.use("/auth", authRotues);
 router.use("/rides", rideRoutes);
+router.use("/requests", requestRoutes);
 export default router;
