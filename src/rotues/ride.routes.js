c0b434b34665736771af2dedc18c1ae7a2e1c2c1
rahmatshah1310 <rahmatshah1310@gmail.com>
2026-02-15 22:38:13 +0500

    feat: introduce ride and ride request features including services, routes, and Drizzle migrations.

--- src/rotues/ride.routes.js
+++ src/rotues/ride.routes.js
@@ -4,6 +4,8 @@ import {
   createRideController,
   getallRidesController,
   updateRideController,
+  getRideByIdController,
+  deleteRideController,
 } from "@/controllers/rides.controller";
 import { authorizeRoles, ensureAuthenticated } from "@/middlewares/auth.mw.js";
 
@@ -19,11 +21,20 @@ router.post(
   authorizeRoles("driver"),
   createRideController,
 );
+
+router.get("/singleRide/:rideId", ensureAuthenticated, getRideByIdController);
 router.put(
   "/updateRide/:rideId",
   ensureAuthenticated,
   authorizeRoles("driver"),
   updateRideController,
+);
+
+router.delete(
+  "/deleteRide/:rideId",
+  ensureAuthenticated,
+  authorizeRoles("driver"),
+  deleteRideController,
 );
 
 export default router;
