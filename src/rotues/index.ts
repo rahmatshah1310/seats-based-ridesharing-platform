c60b323774581a1255f6e9f682cfc003f7716ed0
rahmatshah1310 <rahmatshah1310@gmail.com>
2026-02-18 14:34:30 +0500

    feat: Implement initial database schema and core ride and rating features.

--- src/rotues/index.ts
+++ src/rotues/index.ts
@@ -3,8 +3,10 @@ const router = Express.Router();
 import authRotues from "./auth.routes";
 import rideRoutes from "./ride.routes";
 import requestRoutes from "./request.routes";
+import ratingRoutes from "./rating.routes"
 
 router.use("/auth", authRotues);
 router.use("/rides", rideRoutes);
 router.use("/requests", requestRoutes);
+router.use("/rating",ratingRoutes)
 export default router;
