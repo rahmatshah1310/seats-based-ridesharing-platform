b24e13c606c99c4ef861547db70b27a4050855ad
rahmatshah1310 <rahmatshah1310@gmail.com>
2026-02-23 15:33:16 +0500

    feat: Implement core ride-sharing features including user, ride, booking, rating, complaint, and payment services with database migrations.

--- src/rotues/index.ts
+++ src/rotues/index.ts
@@ -3,10 +3,12 @@ const router = Express.Router();
 import authRotues from "./auth.routes";
 import rideRoutes from "./ride.routes";
 import requestRoutes from "./request.routes";
-import ratingRoutes from "./rating.routes"
+import ratingRoutes from "./rating.routes";
+import paymentRoutes from "./payment.routes";
 
 router.use("/auth", authRotues);
 router.use("/rides", rideRoutes);
 router.use("/requests", requestRoutes);
-router.use("/rating",ratingRoutes)
+router.use("/rating", ratingRoutes);
+router.use("/payments", paymentRoutes);
 export default router;
