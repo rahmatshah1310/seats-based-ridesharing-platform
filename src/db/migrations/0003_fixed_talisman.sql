4e6530ed1aef219a33b09747da3bc23fa55343d0
rahmatshah1310 <rahmatshah1310@gmail.com>
2026-03-01 23:06:15 +0500

    feat: Add Drizzle ORM migrations for the application database schema.

--- src/db/migrations/0003_fixed_talisman.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "rides" ADD COLUMN "date" timestamp NOT NULL;--> statement-breakpoint
-ALTER TABLE "rides" ADD COLUMN "time" timestamp NOT NULL;--> statement-breakpoint
-ALTER TABLE "rides" DROP COLUMN "departure_time";
