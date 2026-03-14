c1c283536ddcb222e742c3c7a26e828d5d55c91f
rahmatshah1310 <rahmatshah1310@gmail.com>
2026-03-14 23:57:29 +0500

    feat:add conversation routes for messages

--- src/rotues/conversation.routes.ts
+++ src/rotues/conversation.routes.ts
@@ -1,12 +1,12 @@
 import {
   createConversation,
-  getConversations,
+  getConversationsController,
 } from "@/controllers/conversation.controller";
 import { ensureAuthenticated } from "@/middlewares/auth.mw";
 import express from "express";
 const router = express.Router();
 
 router.post("/new", ensureAuthenticated, createConversation);
-router.get("/", ensureAuthenticated, getConversations);
+router.get("/", ensureAuthenticated, getConversationsController);
 
 export default router;
