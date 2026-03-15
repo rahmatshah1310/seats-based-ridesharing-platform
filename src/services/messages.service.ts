8f661e29e1d65821a5e1860c5bf81a56da610a06
rahmatshah1310 <rahmatshah1310@gmail.com>
2026-03-15 22:49:21 +0500

    feat: Enhance getMessages function to support pagination and message count retrieval

--- src/services/messages.service.ts
+++ src/services/messages.service.ts
@@ -1,10 +1,29 @@
 import { db } from "@/db/db";
 import { messages } from "@/db/schema/messages.model";
+import { desc, sql } from "drizzle-orm";
 
-export const getMessages = async () => {
+export const getMessages = async (page: number = 1, pageSize: number = 20) => {
   try {
-    const results = await db.select().from(messages);
-    return results;
+    const offset = (page - 1) * pageSize;
+    const results = await db
+      .select()
+      .from(messages)
+      .orderBy(desc(messages.createdAt))
+      .limit(pageSize)
+      .offset(offset);
+
+    const [{ count }] = await db
+      .select({ count: sql<number>`count(*)` })
+      .from(messages);
+    return {
+      messages: results,
+      pagination: {
+        total: Number(count),
+        page,
+        pageSize,
+        totalPages: Math.ceil(Number(count) / pageSize),
+      },
+    };
   } catch (error) {
     throw new Error("Error fetching messages");
   }
