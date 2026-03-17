c0f64955aec8289b6ffa7ca594bce99475d1f781
rahmatshah1310 <rahmatshah1310@gmail.com>
2026-03-18 00:00:19 +0500

    feat:Add routes for conversations and messages

--- src/rotues/index.ts
+++ src/rotues/index.ts
@@ -1,14 +1,19 @@
 import Express from "express";
-const router = Express.Router();
 import authRotues from "./auth.routes";
 import rideRoutes from "./ride.routes";
 import requestRoutes from "./request.routes";
 import ratingRoutes from "./rating.routes";
 import paymentRoutes from "./payment.routes";
+import conversationRoutes from "./conversation.routes";
+import messagesRoutes from "./messages.routes";
+const router = Express.Router();
 
 router.use("/auth", authRotues);
 router.use("/rides", rideRoutes);
 router.use("/requests", requestRoutes);
 router.use("/rating", ratingRoutes);
 router.use("/payments", paymentRoutes);
+router.use("/conversations", conversationRoutes);
+router.use("/messages", messagesRoutes);
+
 export default router;
